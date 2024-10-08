30611a9e74ec4b55fddfdc9eb50bed7b12f13152
victor.linroth.sensmetry <victor.linroth@sensmetry.com>
2024-10-08 17:17:49 +0200

    Tidying up.

--- pytest-health/runner-image/run_script.py
+++ pytest-health/runner-image/run_script.py
@@ -1,20 +1,20 @@
+import sys
+import pytest
 from upath import UPath
 from os import environ
 from subprocess import check_call
-from sys import executable
-from pytest import main
 
-if "REQ_RUNNER_HEALTH" in environ:
-    req_path = UPath(environ["REQ_RUNNER_HEALTH"])
-    with open("requirements.txt", "w") as r:
-        r.write(req_path.read_text())
-    check_call([executable, "-m", "pip", "install", "--break-system-packages", "-r", "requirements.txt"])
+if __name__ == "__main__":
+    if "REQ_RUNNER_HEALTH" in environ:
+        req_path = UPath(environ["REQ_RUNNER_HEALTH"])
+        with open("requirements.txt", "w") as r:
+            r.write(req_path.read_text())
+        check_call([sys.executable, "-m", "pip", "install", "--break-system-packages", "-r", "requirements.txt"])
 
-if "SCRIPT_RUNNER_HEALTH" in environ:
-    script_path = UPath(environ["SCRIPT_RUNNER_HEALTH"])
-    script = script_path.read_text()
-    with open("script.py", "w") as s:
-        s.write(script)
-    main(["script.py"])
-else:
-    print("Failure: Could not find SCRIPT_RUNNER_HEALTH.")
+    if "SCRIPT_RUNNER_HEALTH" in environ:
+        sys.exit(pytest.main(["/app/run_script.py"]))
+    else:
+        sys.exit("Failure: Could not find SCRIPT_RUNNER_HEALTH.")
+
+# For pytest.main()
+exec(UPath(environ["SCRIPT_RUNNER_HEALTH"]).read_text(), globals())
