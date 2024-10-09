b6c8f9bd83379bfb253350d6c46b1da911c46a25
victor.linroth.sensmetry <victor.linroth@sensmetry.com>
2024-10-09 17:15:18 +0200

    Added support for automatic instrumentation by `opentelemetry-instrument`.

--- pytest-health/runner-image/run_script.py
+++ pytest-health/runner-image/run_script.py
@@ -1,20 +1,20 @@
-import sys
-import pytest
-from upath import UPath
-from os import environ
-from subprocess import check_call
+import upath
+import os
 
 if __name__ == "__main__":
-    if "REQ_RUNNER_HEALTH" in environ:
-        req_path = UPath(environ["REQ_RUNNER_HEALTH"])
+    import sys
+    from subprocess import check_call
+    if "RESOURCE_HEALTH_RUNNER_REQUIREMENTS" in os.environ:
+        req_path = upath.UPath(os.environ["RESOURCE_HEALTH_RUNNER_REQUIREMENTS"])
         with open("requirements.txt", "w") as r:
             r.write(req_path.read_text())
-        check_call([sys.executable, "-m", "pip", "install", "--break-system-packages", "-r", "requirements.txt"])
+        check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
 
-    if "SCRIPT_RUNNER_HEALTH" in environ:
-        sys.exit(pytest.main(["/app/run_script.py"]))
+    if "RESOURCE_HEALTH_RUNNER_SCRIPT" in os.environ:
+        from opentelemetry.instrumentation.auto_instrumentation import run
+        sys.exit(run())
     else:
-        sys.exit("Failure: Could not find SCRIPT_RUNNER_HEALTH.")
+        sys.exit("Failure: Could not find RESOURCE_HEALTH_RUNNER_SCRIPT.")
 
-# For pytest.main()
-exec(UPath(environ["SCRIPT_RUNNER_HEALTH"]).read_text(), globals())
+# For pytest
+exec(upath.UPath(os.environ["RESOURCE_HEALTH_RUNNER_SCRIPT"]).read_text(), globals())
