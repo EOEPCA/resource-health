3ae3fdbf680b2792475fc787b5d97126ee704cfa
Tilo Wiklund <tilo.wiklund@sensmetry.com>
2024-10-11 10:43:21 +0200

    Change web deployment to use mTLS

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Client } from "@opensearch-project/opensearch"
-import { BasicAuth } from "@opensearch-project/opensearch/lib/pool/index.js";
+//import { BasicAuth } from "@opensearch-project/opensearch/lib/pool/index.js";
+var fs = require('fs');
 
 const ENDPOINT = 'OPENSEARCH_ENDPOINT';
-const USERNAME = 'OPENSEARCH_USERNAME';
-const PASSWORD = 'OPENSEARCH_PASSWORD';
+//const USERNAME = 'OPENSEARCH_USERNAME';
+//const PASSWORD = 'OPENSEARCH_PASSWORD';
+const CA_PATH = 'CA_PATH';
+const CLIENT_CERT_PATH = 'CLIENT_CERT_PATH';
+const CLIENT_KEY_PATH = 'CLIENT_KEY_PATH';
 
 export default async function Home() {
   return (
@@ -15,13 +19,22 @@ export default async function Home() {
 
 async function MainPage() {
   const endpoint = process.env[ENDPOINT]; // https://localhost:9200, for example
-  const username = process.env[USERNAME];
-  const password = process.env[PASSWORD];
-  if (!endpoint || !username || !password) {
-    throw new Error(`Environment variables ${ENDPOINT}, ${USERNAME}, and ${PASSWORD} must be set`);
+  //const username = process.env[USERNAME];
+  //const password = process.env[PASSWORD];
+  const ca_path = process.env[CA_PATH];
+  const client_cert_path = process.env[CLIENT_CERT_PATH];
+  const client_key_path = process.env[CLIENT_KEY_PATH];
+  if (!endpoint || !ca_path || !client_cert_path || !client_key_path) {
+    throw new Error(`Environment variables ${ENDPOINT}, ${CA_PATH}, ${CLIENT_KEY_PATH}, and ${CLIENT_CERT_PATH} must be set`);
   }
   try {
-    const testResults = await GetTestResults({ endpoint: endpoint, username: username, password: password });
+    const testResults = await GetTestResults({
+      endpoint: endpoint,
+      ca_path: ca_path,
+      client_cert_path: client_cert_path,
+      client_key_path: client_key_path
+    });
+    
     return (
       <table className="table-auto">
         <thead>
@@ -60,13 +73,17 @@ type TestResultInfo = {
   message?: string
 }
 
-async function GetTestResults({ endpoint, ...auth }: { endpoint: string } & BasicAuth): Promise<TestResultInfo[]> {
+async function GetTestResults({ endpoint, ca_path, client_cert_path, client_key_path }: { endpoint: string, ca_path : string, client_cert_path : string, client_key_path : string }): Promise<TestResultInfo[]> {
   const client = new Client({
     node: endpoint,
     suggestCompression: true,
-    auth: auth,
+    //Use mTLS-based authentication
+    //auth: auth,
     ssl: {
-      rejectUnauthorized: false
+      rejectUnauthorized: false,
+      ca: fs.readFileSync(ca_path),
+      cert: fs.readFileSync(client_cert_path),
+      key: fs.readFileSync(client_key_path)
     }
     // use_ssl=True,
     // verify_certs: false,
