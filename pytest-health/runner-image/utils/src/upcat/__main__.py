17977dd80dd4245ec63538f01f06df5cedcc9c2c
victor.linroth.sensmetry <victor.linroth@sensmetry.com>
2024-10-17 12:49:23 +0200

    Replaced python script with a local python package that is installed in the compiler phase of the Docker build.

---
+++ pytest-health/runner-image/utils/src/upcat/__main__.py
@@ -0,0 +1,29 @@
+import argparse
+import importlib
+from upath import UPath
+
+
+def main():
+    parser = argparse.ArgumentParser(description="The upcat utility reads the content from paths sequentially, writing them to the standard output.")
+
+    parser.add_argument(
+            "-v",
+            "--version",
+            action="version",
+            version="%(prog)s " + importlib.metadata.version("upcat")
+    )
+
+    parser.add_argument(
+        "path",
+        nargs="+",
+        help="path to content"
+    )
+
+    args = parser.parse_args()
+
+    for path in args.path:
+        print(UPath(path).read_text())
+
+
+if __name__ == "__main__":
+    main()
