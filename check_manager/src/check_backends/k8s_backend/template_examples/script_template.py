5d1dea0cf8e492885b2755fca8293cbfec3ff881
Tilo Wiklund <tilo.wiklund@sensmetry.com>
2025-04-03 10:04:16 +0200

    simplify script template

--- check_manager/src/check_backends/k8s_backend/template_examples/script_template.py
+++ check_manager/src/check_backends/k8s_backend/template_examples/script_template.py
@@ -1,71 +1,29 @@
-from typing import override
-from pydantic import TypeAdapter
-from kubernetes_asyncio.client.models.v1_cron_job import V1CronJob
-from kubernetes_asyncio.client.models.v1_env_var import V1EnvVar
+from check_backends.k8s_backend.template_utils import *
 
-from api_utils.json_api_types import Json
-from check_backends.check_backend import (
-    CheckTemplate,
-    CheckTemplateAttributes,
-    CheckTemplateId,
-    CheckTemplateMetadata,
-    CronExpression,
+class DefaultK8sArguments(BaseModel):
+    script : str = Field(json_schema_extra={'format':'textarea'})
+    requirements : str|None = Field(json_schema_extra={'format':'textarea'}, default=None)
+
+def default_k8s_template_containers(template_args : DefaultK8sArguments) -> list[Container]:
+    env = {
+        "RESOURCE_HEALTH_RUNNER_SCRIPT": template_args.script
+    }
+
+    if template_args.requirements is not None:
+        env["RESOURCE_HEALTH_RUNNER_REQUIREMENTS"] = template_args.requirements
+    
+    return [
+        container(
+            image=DEFAULT_CONTAINER_IMAGE,
+            env=env
+        )
+    ]
+
+DefaultK8sTemplate = cronjob_template(
+    template_id = "default_k8s_template",
+    argument_type = DefaultK8sArguments,
+    label="Default Kubernetes template",
+    description="Default template for checks in the Kubernetes backend.",
+    # annotations = simple_ping_annotations,
+    containers = default_k8s_template_containers,
 )
-from check_backends.k8s_backend.template_utils import make_base_cronjob
-from check_backends.k8s_backend.templates import CronjobTemplate
-
-
-class DefaultK8sTemplate(CronjobTemplate):
-    @override
-    def get_check_template(self) -> CheckTemplate:
-        return CheckTemplate(
-            id=CheckTemplateId("default_k8s_template"),
-            attributes=CheckTemplateAttributes(
-                metadata=CheckTemplateMetadata(
-                    label="Default Kubernetes template",
-                    description="Default template for checks in the Kubernetes backend.",
-                ),
-                arguments={
-                    "$schema": "http://json-schema.org/draft-07/schema",
-                    "type": "object",
-                    "properties": {
-                        "script": {"type": "string", "format": "textarea"},
-                        "requirements": {"type": "string", "format": "textarea"},
-                    },
-                    "required": [
-                        "script",
-                    ],
-                },
-            ),
-        )
-
-    @override
-    def make_cronjob(
-        self,
-        template_args: Json,
-        schedule: CronExpression,
-    ) -> V1CronJob:
-        script = TypeAdapter(str).validate_python(template_args["script"])
-        requirements: str | None = TypeAdapter(str | None).validate_python(
-            template_args.get("requirements")
-        )
-        cronjob = make_base_cronjob(
-            schedule=schedule,
-        )
-        env = cronjob.spec.job_template.spec.template.spec.containers[0].env or []
-        if script:
-            env.append(
-                V1EnvVar(
-                    name="RESOURCE_HEALTH_RUNNER_SCRIPT",
-                    value=script,
-                )
-            )
-        if requirements:
-            env.append(
-                V1EnvVar(
-                    name="RESOURCE_HEALTH_RUNNER_REQUIREMENTS",
-                    value=requirements,
-                )
-            )
-        cronjob.spec.job_template.spec.template.spec.containers[0].env = env
-        return cronjob
