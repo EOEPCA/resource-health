3ee6cbbe80158b4a2730156473c32998b4a527e0
jonas.puksta.sensmetry <jonas.puksta@sensmetry.com>
2025-06-19 15:38:26 +0300

    add an option to the website to display the data from the last year

--- check_manager_website/src/lib/config.ts
+++ check_manager_website/src/lib/config.ts
@@ -5,7 +5,7 @@ export const telemetryDurationOptions: Duration[] = [
   { days: 1 },
   { weeks: 1 },
   { months: 1 },
-  //   { years: 1 },
+  { years: 1 },
 ];
 
 export const DEFAULT_TELEMETRY_DURATION: Duration = telemetryDurationOptions[1];
